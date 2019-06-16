6c342ca5f2dbd544e07487dcbe6cf50b1ec0cecf
OrlandoLabanGupioc <orlando.laban@unas.edu.pe>
2019-06-16 12:07:10 -0500

    Pimer commit - Estructura base del proyecto, lista para ser utilizada en otros proyectos

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { ModuleWithProviders } from '@angular/compiler/src/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { RegisterComponent } from './login/register.component';
+
+
+
+const appRoutes: Routes = [
+{path: 'login', component: LoginComponent},
+{path: 'register', component: RegisterComponent},
+{path: '**', component: NopagefoundComponent}
+];
+
+export const APP_ROUTES: ModuleWithProviders = RouterModule.forRoot(appRoutes, { useHash: true});
---
+++ src/app/page/pages.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+//Rutas
+import { PAGES_ROUTES } from './pages.routes';
+//Módulos
+import { SharedModule } from '../shared/shared.module';
+
+//Componentes
+import { PagesComponent } from './pages.component';
+import { DashboarComponent } from './dashboar/dashboar.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+@NgModule({
+    declarations:[
+        PagesComponent,
+        DashboarComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    exports:[
+        DashboarComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+
+export class PagesModule{}
